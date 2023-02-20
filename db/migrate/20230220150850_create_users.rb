a9c332ad639c35bfc3618c8d060f052e99360f9a
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-20 12:16:52 -0300

    Fix table name to singular

--- db/migrate/20230220150850_create_users.rb
+++ db/migrate/20230220150850_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration[7.0]
+class CreateUser < ActiveRecord::Migration[7.0]
   def change
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :name
       t.string :photo
       t.text :bio
