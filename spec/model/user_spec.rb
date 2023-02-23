e5367b8164d664d3503b01fd55f3095212fb197b
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-23 17:33:45 -0300

    fix

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,4 +1,4 @@
-#require 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject { User.new(name: 'Luis', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Microverse.', posts_counter: 0) }
