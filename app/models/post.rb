7b042b6a2ca45dde39ddfcadf61803d3d20fdddb
Luis Muñoz <l.munoz.m@outlook.com>
2023-02-28 10:57:47 -0300

    Update Post views.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: 'post', foreign_key: 'author_id'
-  has_many :likes, class_name: 'like', foreign_key: 'author_id'
-  has_many :comments, class_name: 'comment', foreign_key: 'author_id'
+  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
 
   validates :name, presence: true, allow_blank: false
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
