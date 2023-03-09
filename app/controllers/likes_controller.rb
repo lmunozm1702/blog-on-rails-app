3e656bd3ff5a3fdd719a2bfbb4b169019b752e67
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-09 18:59:03 -0300

    Fix likes controller to use current user with devise

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,17 @@
 class LikesController < ApplicationController
   def create
-    @like = User.find(current_user).likes.new(likes_params)
+    @like = User.find(current_user.id).likes.new(likes_params)
     flash[:notice] = @like.errors.full_messages.to_sentence unless @like.save
     Like.update_likes_count(@like.post_id)
-    redirect_back_or_to user_post_path(current_user, @like.post_id)
+    redirect_back_or_to user_post_path(current_user.id, @like.post_id)
   end
 
   def destroy
-    @like = User.find(current_user).likes.find(params[:id])
+    @like = User.find(current_user.id).likes.find(params[:id])
     post = @like.post
     @like.destroy
     Like.update_likes_count(@like.post_id)
-    redirect_back_or_to user_post_path(current_user, post.id)
+    redirect_back_or_to user_post_path(current_user.id, post.id)
   end
 
   private
