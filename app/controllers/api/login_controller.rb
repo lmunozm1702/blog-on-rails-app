a1b28b95d5935d38bb18c2a7d15ace8bdff60e54
Luis Muñoz <l.munoz.m@outlook.com>
2023-03-10 20:15:40 -0300

    Create API controller for comments
    Include query all comments for a post and create new post.

--- app/controllers/api/login_controller.rb
+++ app/controllers/api/login_controller.rb
@@ -3,20 +3,18 @@ require 'jwt'
 class Api::LoginController < Api::ApplicationController
   skip_before_action :authenticate_user
 
-  @@hmac_secret = Rails.application.secrets.secret_key_base
-
   def index
-    @auth_user = User.find_by_email(params[:email])
-    if @auth_user && @auth_user.valid_password?(params[:password])
+    @user = User.find_by_email(params[:email])
+    if @user && @user.valid_password?(params[:password])
       payload = {
-        id: @auth_user.id,
-        name: @auth_user.name,
-        email: @auth_user.email
+        id: @user.id,
+        name: @user.name,
+        email: @user.email
       }
-      token = JWT.encode(payload, @@hmac_secret, 'HS256')
+      token = JWT.encode(payload, 'secret')
       render json: { token: }
     else
-      render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+      render json: { name: 'Fail' }
     end
   end
 end
